915b2b52b86c21d050fbfe5c4c72242381f0e4ee
raf-zig <ziravin@yandex.ru>
2022-12-03 20:57:09 +0500

    Add the playRound function

--- javascript.js
+++ javascript.js
@@ -2,13 +2,35 @@
 function getComputerChoice() {
     let randomNumber = (Math.floor(Math.random() * 3));
     if (randomNumber == 0) {
-        return ('ROCK')
+        return ('ROCK');
     } else if (randomNumber == 1) {
-        return ('PAPER')
+        return ('PAPER');
     } else {
-        return ('SCISSORS')
+        return ('SCISSORS');
     }
 }
+
 const playerSelection = prompt('Enter rock or scissors or paper').toUpperCase();
 const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+        return ('You Lose! Paper beats Rock');
+    } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+        return ('You Win! Rock beats Scissors');
+    } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+        return ('You Lose! Scissors beats Paper');
+    } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+        return ('You Win! Paper beats Rock');
+    } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+        return ('You Lose! Rock beats Scissors');
+    } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return ('You Win! Scissors beats Paper ');
+    } else {
+        return (`No one won. Your choice - ${playerSelection}, computer choice - ${computerSelection}`);
+    }
+};
+
+
+
 
