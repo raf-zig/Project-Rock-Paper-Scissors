5bea94720766738539a6c35a73db8ff73fcea75b
raf-zig <ziravin@yandex.ru>
2022-12-11 22:28:59 +0500

    Add variables and functions to a JS file

--- javascript.js
+++ javascript.js
@@ -1,6 +1,37 @@
-for (let i = 0; i < 5; i++) {
+const rock = document.querySelector('#btn1');
+const paper = document.querySelector('#btn2');
+const scissors = document.querySelector('#btn3');
+const para = document.querySelector('p');
+const user = document.querySelector('#user');
+const comp = document.querySelector('#comp');
+const winner = document.querySelector('#winner');
+const finish = document.querySelector('#finish');
+const buttons = document.querySelectorAll('.btn');
+const start = document.querySelector ('#start')
+const button =  document.querySelector('#btn')
+let user_score = 0;
+let comp_score = 0;
 
+rock.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const score = (playRound("ROCK", computerSelection));
+  para.textContent = score
+  display_score(para.textContent) 
+});
 
+paper.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const score = (playRound("PAPER", computerSelection));
+  para.textContent = score
+  display_score(para.textContent) 
+});
+
+scissors.addEventListener('click', () => {
+  const computerSelection = getComputerChoice();
+  const score = (playRound("SCISSORS", computerSelection));
+  para.textContent = score
+  display_score(para.textContent) 
+});
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
@@ -14,7 +45,7 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
         return ('You Lose! Rock beats Scissors');
     } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-        return ('You Win! Scissors beats Paper ');
+        return ('You Win! Scissors beats Paper');
     } else {
         return (`No one won. Your choice - ${playerSelection}, computer choice - ${computerSelection}`);
     }
@@ -22,22 +53,43 @@ function playRound(playerSelection, computerSelection) {
 function getComputerChoice() {
     let randomNumber = (Math.floor(Math.random() * 3));
     if (randomNumber == 0) {
-        return ('ROCK');
+      return ('ROCK');
     } else if (randomNumber == 1) {
         return ('PAPER');
     } else {
-        return ('SCISSORS');
+      return ('SCISSORS');
     }
 };
 
-const playerSelection = prompt('Enter rock or scissors or paper').toUpperCase();
-const computerSelection = getComputerChoice();
-
-function game() {
-  console.log(playRound(playerSelection, computerSelection))
-};
-game();
+function display_score(para) {
+  if(para === 'You Win! Rock beats Scissors' || para ===  'You Win! Paper beats Rock' || para === 'You Win! Scissors beats Paper'){
+    user_score++;
+    user.textContent = user_score
+    if(user_score === 5) {
+      finish.textContent = 'You Win!';
+      buttons.forEach(el => el.disabled = true)
+      start.textContent = 'If you want to play again, press start'
+      but()
+    }
+  } else if ( para === 'You Lose! Paper beats Rock' || para === 'You Lose! Scissors beats Paper' || para === 'You Lose! Rock beats Scissors') {
+    comp_score++
+    comp.textContent = comp_score
+    if(comp_score === 5) {
+      finish.textContent = 'Computer Win!';
+      buttons.forEach(el => el.disabled = true)
+      start.textContent = 'If you want to play again, press start'
+      but()
+    }
+  }
 }
+
+function but(){
+  button.style.visibility = "visible";
+}
+
+
+
+
 
 
 
